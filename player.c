606b92ee53b12c4d9955fc7ac06b7b4e7480beec
jaredasch1 <jaredasch1@gmail.com>
2018-10-02 18:20:55 -0400

    formatting

--- player.c
+++ player.c
@@ -3,33 +3,33 @@
 #include <time.h>
 
 struct player {
-	int hp;
-	char * name;
+    int hp;
+    char * name;
 };
 
 struct player random_player(char* name){
-	struct player p;
-	p.hp = rand() % 900 + 100;
-	p.name = name;
-	return p;
+    struct player p;
+    p.hp = rand() % 900 + 100;
+    p.name = name;
+    return p;   
 }
 
 // Return HP if still living, 0 if dead
 int attack(struct player * p, int damage){
-	printf("Attacking %s for %d damage\n", p->name, damage);
+    printf("Attacking %s for %d damage\n", p->name, damage);
     p->hp -= damage;
-	if(p->hp <= 0){
-		p->hp = 0;
-	}
-	return p->hp;
+    if(p->hp <= 0){
+        p->hp = 0;
+    }
+    return p->hp;
 }
 
 void print_player(struct player p){
-	printf("Player %s: %d HP\n", p.name, p.hp);  
+    printf("Player %s: %d HP\n", p.name, p.hp);     
 }
 
 int main(){
-	srand(time(NULL));
+    srand(time(NULL));
 
     struct player p1 = random_player("Jared");
     struct player p2 = random_player("Mark");
@@ -58,7 +58,7 @@ int main(){
     printf("Expecting Mark to be dead :(\n");
     print_player(p2);
 
-	return 0;
+    return 0;
 }
 
 
